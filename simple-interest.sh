1bd8aed412cdcaaa05d250e844f433c1e86ce796
Manuel-lozano188 <jose.lozano2487@alumnos.udg.mx>
2025-02-12 18:10:25 -0600

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script para ejecutar el cálculo de interés simple en Python
+# Mostrar un mensaje al inicio
+echo "Iniciando el cálculo del interés simple..."
+
+# Ejecutar el script de Python
+python3 calcular_interes_simple.py
+
+# Mensaje al final
+echo "Cálculo completado."
